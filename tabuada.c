b91f7637bd27c23f09592203d848579490b9e3ff
Johana Pizarro <102596180+JohanaPizarroL@users.noreply.github.com>
2024-06-07 23:59:45 -0300

    1078 - Tabuada

---
+++ tabuada.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(){
+    int n;
+    scanf("%d", &n);
+        for(int j = 1; j <= 10; j++){
+            printf("%d x %d = %d\n", j, n, j * n);
+        }
+    return 0;
+}
